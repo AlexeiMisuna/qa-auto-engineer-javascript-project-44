ea4c0cce0840dd3f47ce861192857842fe64abaf
AlekseyMisuna <alexei.misuna@yandex.ru>
2025-08-08 22:20:21 +0500

    update caclGame.js

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import helpers from '../helpers/index.js'
@@ -8,26 +10,31 @@ export const calculatorGame = () => {
   console.log(helpers.rules.calculatorGame)
 
   for (let i = 0; i < 3; i++) {
-    const firstOperand = helpers.getRandomNumber(1, 100)
-    const secondOperand = helpers.getRandomNumber(1, 100)
-    const getRandomOperator = () => {
-      const operators = ['+', '-', '*']
-      const randomIndex = Math.floor(Math.random() * operators.length)
-      return operators[randomIndex]
+    const generateQuestion = () => {
+      const number1 = helpers.getRandomNumber(1, 100)
+      const number2 = helpers.getRandomNumber(1, 100)
+      const operations = ['+', '-', '*']
+      const randomIndex = Math.floor(Math.random() * operations.length)
+      const operator = operations[randomIndex]
+
+      const questionString = `Question: ${number1} ${operator} ${number2}`
+      return { questionString, number1, number2, operator }
     }
-    const operator = getRandomOperator()
-    console.log(`Question: ${firstOperand} ${operator} ${secondOperand}`)
+
+    const { questionString, number1, number2, operator } = generateQuestion()
+    console.log(questionString)
+
     const answer = helpers.getAnswer()
     const numericAnswer = Number(answer)
 
-    const expression = `${firstOperand} ${operator} ${secondOperand}`
+    const expression = `${number1} ${operator} ${number2}`
     const correctAnswer = evaluate(expression)
 
     if (numericAnswer === correctAnswer) {
       console.log(helpers.rules.correctAnswer)
     }
     else {
-      console.log(`'${answer}${helpers.rules.uncorrectAnswer}${correctAnswer}'.`)
+      console.log(`'${answer}${helpers.rules.uncorrectAnswer}${correctAnswer}'.`);
       helpers.tryAgain()
       return
     }
